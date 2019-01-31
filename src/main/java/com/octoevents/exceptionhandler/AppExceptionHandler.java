0b405fd88556ba370759cf8ba739a7aa281cab24
Karielly <karielly.kari@gmail.com>
2019-01-31 18:41:10 -0200

    added media type exception

--- src/main/java/com/octoevents/exceptionhandler/AppExceptionHandler.java
+++ src/main/java/com/octoevents/exceptionhandler/AppExceptionHandler.java
@@ -2,18 +2,15 @@ package com.octoevents.exceptionhandler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -21,32 +18,23 @@ import com.octoevents.exception.MessageException;
 
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
-import javax.servlet.http.HttpServletRequest;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 
 @ControllerAdvice
-@RestController
 public class AppExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	@Autowired
     private MessageSource messageSource;
-	
-	@Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+    
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
+			HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         String messageUser = messageSource.getMessage("message.invalid.properties", null, LocaleContextHolder.getLocale());
-        String messageDev = ex.getCause() != null ? ex.getCause().toString(): ex.toString();
-        return handleExceptionInternal(ex, MessageException.of(messageUser, messageDev), headers, status, request);
-    }
-
-    @ExceptionHandler({EmptyResultDataAccessException.class})
-    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
-        String messageUser = messageSource.getMessage("resource.not-found", null, LocaleContextHolder.getLocale());
         String messageDev = ex.toString();
-        return handleExceptionInternal(ex, MessageException.of(messageUser, messageDev), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+        return handleExceptionInternal(ex, MessageException.of(messageUser, messageDev), new HttpHeaders(), HttpStatus.UNSUPPORTED_MEDIA_TYPE, request);
     }
 
     @ExceptionHandler({ DataIntegrityViolationException.class } )
